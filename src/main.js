248eb58bfa29a8289924ecf7f72e07becca994cc
fabo777 <mario.fabijanec@gmail.com>
2024-05-20 08:41:08 +0200

    hide vuepreloader css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import "./assets/main.css";
 import "primevue/resources/themes/aura-light-green/theme.css";
 import "primevue/resources/primevue.min.css";
-import "../node_modules/vue-preloader/dist/style.css";
 
 import { createApp } from "vue";
 import PrimeVue from "primevue/config";
@@ -12,6 +11,7 @@ import { MotionPlugin } from "@vueuse/motion";
 import Dropdown from "primevue/dropdown";
 import SelectButton from "primevue/selectbutton";
 
+/* import "../node_modules/vue-preloader/dist/style.css"; */
 import Image from "primevue/image";
 
 import VueLazyload from "vue-lazyload";
